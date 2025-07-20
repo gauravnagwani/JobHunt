1e89f268bec9cb8ef04c38cebfab8647bc175da8
Gaurav Nagwani <gauravnagwani859@gmail.com>
2025-07-20 23:53:56 +0530

    Connected database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
 const app = express();
 
 
@@ -14,7 +17,8 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT,(req,res)=>{
+    connectDB();
     console.log(`Server running at port ${PORT}`);
 });
