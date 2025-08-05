439b0f4c5bd94d399daab2a574b1d47cd71d59f2
Gaurav Nagwani <gauravnagwani859@gmail.com>
2025-08-05 21:06:21 +0530

    Added user route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js"; 
+
 dotenv.config({});
 const app = express();
 
@@ -12,12 +14,18 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin : 'http//localhost:5173',
+    origin : 'http://localhost:5173',
     credentials : true
 }
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
+
+//api routes
+app.use("/api/v1/user", userRoute);
+
+
+
 app.listen(PORT,(req,res)=>{
     connectDB();
     console.log(`Server running at port ${PORT}`);
