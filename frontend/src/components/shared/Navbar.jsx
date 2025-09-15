5ad3d5a9d7a4592b305579c9be919953bc3a84b6
Gaurav Nagwani <gauravnagwani859@gmail.com>
2025-09-15 21:54:31 +0530

    React  + Shadcn UI + Tailwind CSS for frontend

---
+++ frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-white'>
+      <div>
+        <h1 className='font-bold text-2xl'>Job<span className='text-[#F83002]'>Portal</span></h1>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
